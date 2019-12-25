b863c15994b1ca1ca4d105476af8b7612f2ec948
phiiileo <emmanuelboluwajib1@gmail.com>
2019-12-25 19:00:40 +0100

    Updates

---
+++ precache-manifest.d82df5ce79a29b043a7bef58f7b98b07.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c4acfcd45c854a9f06e98a9932b10df",
+    "url": "/myreactapp/index.html"
+  },
+  {
+    "revision": "f36b67543aa8aecc3d94",
+    "url": "/myreactapp/static/js/2.8af07b9d.chunk.js"
+  },
+  {
+    "revision": "eba862332f359d117976",
+    "url": "/myreactapp/static/js/main.80185bde.chunk.js"
+  },
+  {
+    "revision": "cd46fae6a96cfbae52bc",
+    "url": "/myreactapp/static/js/runtime-main.d338ddc3.js"
+  }
+]);
